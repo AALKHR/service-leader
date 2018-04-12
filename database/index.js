bb6e852041bb28a708393832c05b4563f8b39a5e
Leader Le <leader@Leaders-MacBook-Pro.local>
2018-04-12 11:38:16 -0700

    database setup, seeding file setup

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/chairbnb';
+const mongoUri = 'mongodb://localhost:27017/chairbnb';
 
 const db = mongoose.connect(mongoUri);
 
