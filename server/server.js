46b999051edc7e5295ba5d155e257008f9e1dae4
Leader Le <leader@Leaders-MacBook-Pro.local>
2018-05-10 19:36:25 -0700

    adjusted port variable to accept environment port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const Listing = require('../database/Listing.js');
 
 const app = express();
-const port = 3009;
+const port = process.env.PORT || 3009;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
